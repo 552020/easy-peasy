829b5898a686117df5256074203572abcd60d5cc
552020 <stefanolombardo@posteo.de>
2021-08-14 21:50:55 +0200

    second commit

---
+++ src/components/SimpleInput.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import gif from "../assets/nope.gif";
+
+const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+  const nameInputOnBlurHandler = () => {
+    setEnteredNameTouched(true);
+  };
+
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+
+    setEnteredNameTouched(true);
+
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    setEnteredName(enteredName);
+
+    setEnteredName("");
+    setEnteredNameTouched(false);
+  };
+
+  const nameInputClasses = !nameInputIsInvalid
+    ? "form-control"
+    : "form-control invalid";
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          onBlur={nameInputOnBlurHandler}
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+          type="text"
+          id="name"
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty!</p>
+        )}
+        {nameInputIsInvalid && <img src={gif} alt="nope" />}
+      </div>
+
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
